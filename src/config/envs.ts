5d315c6c89c2021b041a9f9a62344d939ba6f8bb
Sabino Hdz <sakuraeyes21@gmail.com>
2026-01-31 17:12:40 -0600

    NAT server configuration

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,15 +4,20 @@ import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
+  NATS_SERVERS: string[];
 }
 
 const envsSchema = joi
   .object({
     PORT: joi.number().required(),
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
   })
   .unknown(true);
 
-const { error, value } = envsSchema.validate(process.env);
+const { error, value } = envsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
@@ -22,4 +27,5 @@ const envsVars: EnvVars = value as EnvVars;
 
 export const envs = {
   port: envsVars.PORT,
+  natsServers: envsVars.NATS_SERVERS,
 };
